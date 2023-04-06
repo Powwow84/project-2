7779f986c5d17a65620b4b16adc3f171e4914911
Powwow84 <pao.saelee84@gmail.com>
2023-04-06 12:28:45 -0700

    added server and routes

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT || 8000
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended: false}))
+app.use('/users', require('./controllers/users.js'))
+
+app.get('/', (req, res)=> {
+    res.send('welcome to the auth api')
+})
+
+app.listen(PORT, ()=> {
+    console.log(`authenticating users on port ${PORT}`)
+})
