d4998fd33f6ffb091a126986b1969f8003928063
Powwow84 <pao.saelee84@gmail.com>
2023-04-08 12:54:15 -0700

    started adding some css to templatize the views

--- controllers/fish.js
+++ controllers/fish.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 const db = require('../models')
 
-router.get('/fish', (req, res) => {
-    res.render('fish/fish.ejs')
-    console.log('this should have worked')
+router.get('/fish', async(req, res) => {
+    const fish = await db.fish.findAll()
+    res.render('fish/fish.ejs', {
+        fish:fish,
+    })
 })
 
 
