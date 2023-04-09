278b715a5f3b63500ed514575c0343c49ab18893
Powwow84 <pao.saelee84@gmail.com>
2023-04-08 20:22:22 -0700

    Fish search is working but the bug where I cant find similar items is stupid. need a break

--- controllers/fish.js
+++ controllers/fish.js
@@ -2,13 +2,28 @@ const express = require('express')
 const router = express.Router()
 const db = require('../models')
 
-router.get('/fish', async(req, res) => {
+router.get('/', async(req, res) => {
     const fish = await db.fish.findAll()
     res.render('fish/fish.ejs', {
         fish:fish,
     })
 })
 
+
+router.get('/show', async (req, res) => {
+    try {
+      const species = await db.fish.findAll({
+        where: {
+          name: req.query.fishSearch
+        },
+      });
+      res.render('fish/show.ejs', {
+        species: species,
+      });
+    } catch (err) {
+      console.log('Oops That didnt work');
+    }
+  });
 
 
 
