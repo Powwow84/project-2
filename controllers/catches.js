cfdd3d3abdbf615bddc190cd4800cef737a7589d
Powwow84 <pao.saelee84@gmail.com>
2023-04-09 15:04:27 -0700

    added the add catch screen

--- controllers/catches.js
+++ controllers/catches.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const db = require('../models')
 
-
+router.get("/add", (req, res) => {
+    res.render("catches/add.ejs")
+})
 
 
 
