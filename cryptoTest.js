aa1de886df9bee2874c4f5fb5d748f346dea65a0
Powwow84 <pao.saelee84@gmail.com>
2023-04-09 00:07:07 -0700

    seeded the catch table, there is something buggy with the name

--- cryptoTest.js
+++ cryptoTest.js
@@ -19,10 +19,26 @@
 
 // const cryptoJs = require('crypto-js')
 
-const stringToEncrypt = 'hello i am a secret message'
-const encryptionKey = 'myKey'
+// const stringToEncrypt = 'hello i am a secret message'
+// const encryptionKey = 'myKey'
 
-const myEncryption = cryptoJs.AES.encrypt(stringToEncrypt, encryptionKey)
-console.log(myEncryption)
-const decryptedMessage = cryptoJs.AES.decrypt(myEncryption.toString(), encryptionKey)
-console.log(decryptedMessage.toString(cryptoJs.enc.Utf8))
+// const myEncryption = cryptoJs.AES.encrypt(stringToEncrypt, encryptionKey)
+// console.log(myEncryption)
+// const decryptedMessage = cryptoJs.AES.decrypt(myEncryption.toString(), encryptionKey)
+// console.log(decryptedMessage.toString(cryptoJs.enc.Utf8))
+const axios = require("axios")
+const db = require('./models')
+
+db._catch.create({
+    userId: 2,
+    fishId: 1,
+    catch_name: "huge tuna", 
+    length: 10,
+    weight: 10,
+    description: "it was an awesome",
+    img: 'https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1920,f_auto/DCTM_Penguin_UK_DK_AL644648_p7nd0z.jpg'
+  }).then(_catch => {
+    console.log(_catch);
+  }).catch(err => {
+    console.log(err);
+  });
